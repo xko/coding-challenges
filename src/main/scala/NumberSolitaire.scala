f95fda9910b3fa8797e7388d5cfe0b1b85d1e3df
Niko Ugarov <xxkkoo@gmail.com>
2020-10-10 15:12:52 +0200

    NumberSolitaire in scala
    https://app.codility.com/programmers/lessons/17-dynamic_programming/number_solitaire/
    https://app.codility.com/demo/results/trainingGE27KH-YHZ/

---
+++ src/main/scala/NumberSolitaire.scala
@@ -0,0 +1,17 @@
+// Task:   https://app.codility.com/programmers/lessons/17-dynamic_programming/number_solitaire/
+// Result: https://app.codility.com/demo/results/trainingGE27KH-YHZ/
+
+object Solution {
+
+  case class Game(a: Array[Int], till: Int, before: Array[Int] = Array()) {
+    def next(): Game = Game(a, till + 1, (before :+ this.answer).takeRight(6))
+
+    val answer: Int = if(before.isEmpty) a(till) else before.map(_ + a(till)).max
+  }
+
+
+  def solution(a: Array[Int]): Int = {
+    val res = (1 until a.length).foldLeft ( Game(a, 0) )((game, _) => game.next() )
+    res.answer
+  }
+}
