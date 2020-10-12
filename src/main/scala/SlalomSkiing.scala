27c2e725536fd73f5f2c46a4999675b557f63b6a
Niko Ugarov <xxkkoo@gmail.com>
2020-10-12 23:05:13 +0200

    SlalomSkiing: Int to Long - now 100% correct
    but still slow - 61%
    https://app.codility.com/demo/results/trainingBS5KPC-2KM/

--- src/main/scala/SlalomSkiing.scala
+++ src/main/scala/SlalomSkiing.scala
@@ -1,16 +1,19 @@
 package  SlalomSkiing
 
+//Task:  https://app.codility.com/programmers/lessons/90-tasks_from_indeed_prime_2015_challenge/slalom_skiing/
+//Result:https://app.codility.com/demo/results/trainingBS5KPC-2KM/
+
 import scala.collection.SortedSet
 
 
 object Solution {
 
-  case class LIS(tails: SortedSet[Int] = SortedSet.empty) {
-    def + (nextEl: Int): LIS = copy(tails.until(nextEl) + nextEl ++ tails.from(nextEl).drop(1))
+  case class LIS(tails: SortedSet[Long] = SortedSet.empty) {
+    def + (nextEl: Long): LIS = copy(tails.until(nextEl) + nextEl ++ tails.from(nextEl).drop(1))
     def size: Int = tails.size
   }
 
-  def solve(i: Int, N:Int, a: Int=>Int, turnsToGo: Int, lis: LIS = LIS()): Int = {
+  def solve(i: Int, N:Int, a: Int=>Long, turnsToGo: Int, lis: LIS = LIS()): Int = {
     if(i >= N) lis.size
     else {
       val straight = solve(i+1,N,a,turnsToGo,lis+a(i))
@@ -22,6 +25,6 @@ object Solution {
   }
 
   def solution(a: Array[Int]): Int = {
-    solve(0,a.length,a,2)
+      solve(0,a.length,i=>a(i) ,2)
   }
 }
