71845b1bf080b2a79799fd38c91a34e2067c0423
Niko Ugarov <xxkkoo@gmail.com>
2020-10-16 21:30:54 +0200

    SlalomSkiing: Directly tracking (0,1,2)-turn subsequences, introduced DLong
    DLong allows the Set to hold same values, sorted by additional delta.
    Performance improved a bit - 69%

--- src/main/scala/SlalomSkiing.scala
+++ src/main/scala/SlalomSkiing.scala
@@ -1,30 +1,56 @@
 package  SlalomSkiing
 
 //Task:  https://app.codility.com/programmers/lessons/90-tasks_from_indeed_prime_2015_challenge/slalom_skiing/
-//Result:https://app.codility.com/demo/results/trainingBS5KPC-2KM/
+//Result:https://app.codility.com/demo/results/trainingDNG57N-69J/
 
 import scala.collection.SortedSet
+import scala.language.implicitConversions
 
 
 object Solution {
 
-  case class LIS(tails: SortedSet[Long] = SortedSet.empty) {
-    def + (nextEl: Long): LIS = copy(tails.until(nextEl) + nextEl ++ tails.from(nextEl).drop(1))
-    def size: Int = tails.size
+  case class DLong(v:Long, d: Int=0) extends Ordered[DLong] {
+
+    override def compare(that: DLong): Int = if(v==that.v) d.compare(that.d) else v.compare(that.v)
+
+    def nudge(dd: Int) : DLong = copy(v,d+dd)
   }
 
-  def solve(i: Int, N:Int, a: Int=>Long, turnsToGo: Int, lis: LIS = LIS()): Int = {
-    if(i >= N) lis.size
+  val Inc:SortedSet[DLong] = SortedSet.empty
+  val Dec:SortedSet[DLong] = SortedSet.empty(Ordering[DLong].reverse)
+
+  implicit def llong(v: Long) = DLong(v)
+
+  case class LIS(tails: SortedSet[DLong] ) {
+    val decrement:Int = tails.ordering.min(1,-1).v.toInt
+
+    def + (nextEl: Long): LIS = copy(tails.until(nextEl) + nextEl ++ tails.from(nextEl).drop(1))
+    def size: Int = tails.size
+
+    def fill(to:DLong, fromSize: Int, toSize: Int): Seq[DLong] =  1.until(toSize - fromSize).map(_ * decrement).map(to.nudge)
+
+    def ++ (incoming: LIS): LIS = {
+      val insert = incoming.tails.last
+      val prefix = tails.until(insert)
+      val filler = fill(insert, prefix.size, incoming.size)
+      val rest = tails.drop(prefix.size+filler.size+1)
+      copy(prefix ++ filler + insert ++ rest)
+    }
+
+  }
+
+
+  def solve(i: Int, N:Int, a: Int=>Long, straight:LIS = LIS(Inc), turn1:LIS = LIS(Dec), turn2:LIS = LIS(Inc)): Int = {
+    if(i >= N) turn2.size
     else {
-      val straight = solve(i+1,N,a,turnsToGo,lis+a(i))
-      if (turnsToGo == 0) straight else {
-        val turned = solve(i + 1, N, j => 2 * a(i) - a(j), turnsToGo - 1, lis + a(i))
-        Math.max(straight, turned)
-      }
+      val s =  straight +  a(i)
+      val t1 = turn1    +  a(i) ++ s
+      val t2 = turn2    +  a(i) ++ t1
+      solve(i+1, N, a, s, t1, t2)
     }
   }
 
   def solution(a: Array[Int]): Int = {
-      solve(0,a.length,i=>a(i) ,2)
+      solve(0,a.length,i=>a(i))
   }
 }
