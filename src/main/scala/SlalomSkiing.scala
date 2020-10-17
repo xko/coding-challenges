0e352310b0b4bc17cfe58aac5157adc233bad91f
Niko Ugarov <xxkkoo@gmail.com>
2020-10-18 01:12:02 +0200

    SlalomSkiing: clean and straightforward implementation
    No more hacky DLong, better naming.
    Didn't get any faster though - still 69%

--- src/main/scala/SlalomSkiing.scala
+++ src/main/scala/SlalomSkiing.scala
@@ -1,54 +1,57 @@
 package  SlalomSkiing
 
 //Task:  https://app.codility.com/programmers/lessons/90-tasks_from_indeed_prime_2015_challenge/slalom_skiing/
-//Result:https://app.codility.com/demo/results/trainingDNG57N-69J/
+//Result:https://app.codility.com/demo/results/trainingS7QTJR-XGZ/
 
 import scala.collection.Searching._
 import scala.math.Ordering
 
 object Solution {
 
-  case class DLong(v:Long, d: Int=0) extends Ordered[DLong] {
-    override def compare(that: DLong): Int = if(v==that.v) d.compare(that.d) else v.compare(that.v)
-    def nudge(dd: Int) : DLong = copy(v,d+dd)
+  case class Candidate(tip: Long, len: Int )
+  implicit def candidate1(tip:Long) = Candidate(tip, 1)
+
+  val Inc:   Ordering[Candidate] = Ordering.by(_.tip)
+  val Dec:   Ordering[Candidate] = Inc.reverse
+  val ByLen: Ordering[Candidate] = Ordering.by(_.len)
+
+  val empty:IndexedSeq[Candidate] = Vector()
+
+  implicit class Candidates(self: IndexedSeq[Candidate]) {
+    def len: Int = self.lastOption.fold(0)(_.len)
+
+    def splitWhere(v: Candidate)(implicit ord: Ordering[Candidate]) = self.splitAt(self.search(v).insertionPoint)
+
+    def dropTo(v: Candidate)(implicit ord: Ordering[Candidate]) = self.search(v) match {
+      case Found(i) => self.drop(i+1)
+      case InsertionPoint(i) => self.drop(i)
+    }
   }
 
-  val minus0 = DLong(0,-1)
-  val plus0 = DLong(0,1)
-
-
-  val Inc = Ordering[DLong]
-  val Dec = Ordering[DLong].reverse
-  val empty:IndexedSeq[DLong] = Nil.toIndexedSeq
-
-  case class LIS(tails: IndexedSeq[DLong], implicit val ordering: Ordering[DLong]) {
-    val decrement:Int = ordering.min(minus0,plus0).d
+  case class LIS(candidates: IndexedSeq[Candidate], implicit val ordering: Ordering[Candidate]) {
 
     def + (nextEl: Long): LIS ={
-      val ip = tails.search(DLong(nextEl)).insertionPoint
-      copy((tails.slice(0, ip) :+ DLong(nextEl)) ++ tails.drop(ip+1))
+      val (prefix,rest) = candidates.splitWhere(nextEl)
+      val insert = Candidate(nextEl, prefix.len + 1)
+      copy( (prefix :+ insert) ++ rest.dropTo(insert)(ByLen) )
     }
-    def size: Int = tails.size
 
-    def fill(to:DLong, fromSize: Int, toSize: Int): Seq[DLong] =  1.until(toSize - fromSize).map(_ * decrement).map(to.nudge)
+    def size: Int = candidates.len
 
     def ++ (incoming: LIS): LIS = {
-      val insert = incoming.tails.last
-      val ip = tails.search(insert).insertionPoint
-      val prefix = tails.slice(0, ip)
-      val filler = fill(insert, prefix.size, incoming.size)
-      val rest = tails.drop(prefix.size+filler.size+1)
-      copy((prefix ++ filler :+ insert) ++ rest)
+      val insert = incoming.candidates.last
+      val (prefix,rest) = candidates.splitWhere(insert)
+      if (prefix.len >= insert.len) this
+      else copy( (prefix :+ insert) ++ rest.dropTo(insert)(ByLen) )
     }
-
   }
 
   def solve(i: Int, N:Int, a: Int=>Long, straight:LIS = LIS(empty,Inc), turn1:LIS = LIS(empty,Dec), turn2:LIS = LIS(empty,Inc)): Int = {
     if(i >= N) turn2.size
     else {
       val s =  straight +  a(i)
-      val t1 = if(s.size > straight.size) turn1 ++ s  else turn1 + a(i)
-      val t2 = if(t1.size > turn1.size)   turn2 ++ t1 else turn2 + a(i)
+      val t1 = if(s.size > straight.size) turn1 + a(i) ++ s  else turn1 + a(i)
+      val t2 = if(t1.size > turn1.size)   turn2 + a(i) ++ t1 else turn2 + a(i)
       solve(i+1, N, a, s, t1, t2)
     }
   }
